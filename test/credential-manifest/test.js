5ecd84e20589f240928faacf46c0ea7075e76b59
Gabe Cohen <gcohen@squareup.com>
2022-05-31 11:32:36 -0700

    move scheamas to to plevel folder

--- test/credential-manifest/test.js
+++ test/credential-manifest/test.js
@@ -6,7 +6,7 @@ const ajv = require('../__fixtures__/ajv');
 describe('Credential Manifest', function () {
   describe('JSON Schema', function () {
     it('should validate the basic example object using JSON Schema Draft 7', function () {
-      const schema = JSON.parse(fs.readFileSync(__dirname + '/schema.json'));
+      const schema = JSON.parse(fs.readFileSync(__dirname + '/../../schemas/credential-manifest.json'));
       const data = JSON.parse(fs.readFileSync(__dirname + '/all_features.json'));
       const validate = ajv.compile(schema);
       const valid = validate(data);

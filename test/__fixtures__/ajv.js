81ad59348e08b8cdc37e079195e978923def2e17
Gabe Cohen <gcohen@squareup.com>
2022-07-01 10:01:42 -0700

    update pe claim format schemas

--- test/__fixtures__/ajv.js
+++ test/__fixtures__/ajv.js
@@ -420,6 +420,36 @@ const schemas = {
         }
       }
     }
+  },
+  "https://identity.foundation/claim-format-registry/schemas/presentation-definition-claim-format-designations.json": {
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "title": "Presentation Definition Claim Format Designations",
+    "type": "object",
+    "additionalProperties": false,
+    "patternProperties": {
+      "^jwt$|^jwt_vc$|^jwt_vp$": {
+        "type": "object",
+        "additionalProperties": false,
+        "properties": {
+          "alg": {
+            "type": "array",
+            "minItems": 1,
+            "items": { "type": "string" }
+          }
+        }
+      },
+      "^ldp_vc$|^ldp_vp$|^ldp$": {
+        "type": "object",
+        "additionalProperties": false,
+        "properties": {
+          "proof_type": {
+            "type": "array",
+            "minItems": 1,
+            "items": { "type": "string" }
+          }
+        }
+      }
+    }
   }
 }
 
